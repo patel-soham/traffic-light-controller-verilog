a5855ebc14d6324798455ec8665dfe71c00feb70
Soham Patel <40416883+patel-soham@users.noreply.github.com>
2022-11-05 13:21:11 -0400

    Update tlc.v

--- tlc.v
+++ tlc.v
@@ -1,14 +1,31 @@
+// Traffic Light Controller 
+
+/* 
+INPUTS
+	1. clk - Clock
+	2. rst - Reset
+	3. addr - Address bus used while programming registers by master
+	4. data - Data bus to set values of registers by master
+	5. valid - Singal from master to indicate master is ready for data transfer
+
+OUTPUTS
+	1. ready - Signal from TLC to indicate TLC is also ready for data transfer
+	2. state - output signal bus to indicate current light state 
+*/
 module tlc (clk, rst, addr, data, valid, ready, state);
 
 parameter FREQ = 0.001, // input clock frequency in MHz 
 
 		  ADDR_WIDTH = 3, //Min address bits required for 3 registers is 2. 
+		  DATA_WIDTH = 8, // Keep min data width to be 8 to program 3 registers with time delay value in seconds 
+		  
+		  // Internal addresses of each register
+		  // Each registers stores values in seconds that can be configured by Master
 		  ADDR_RED = 0, 
 		  ADDR_YELLOW = 1,
 		  ADDR_GREEN = 2,
-
-		  DATA_WIDTH = 8, // Keep min data width to be 8 to program 3 registers with time delay value in seconds 
 		  
+		  // States
 		  RESET = 2'b00,
 		  RED = 2'b01,
 		  YELLOW = 2'b10,
